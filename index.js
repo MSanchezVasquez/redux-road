93781583068b97729b075ead7dcd7653daccbf7b
MSanchezVasquez <moisesup.25@gmail.com>
2025-07-26 22:14:02 -0500

    feat(reducer): implement 'gather' action logic in wagonReducer

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const initialWagonState = {
 // Redux reducer function to manage the wagon's state
 const wagonReducer = (state = initialWagonState, action) => {
   switch (action.type) {
+    case "gather": {
+      return {
+        ...state,
+        supplies: state.supplies + 15,
+        distance: state.distance,
+        days: state.days + 1,
+      };
+    }
     default: {
       return state; // Return the current state for unhandled actions
     }
